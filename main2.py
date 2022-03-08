6da18575e34fab30d47e82797d9e23744a9d03e8
unknown <volkov.vadym@gmail.com>
2022-03-08 20:10:43 +0200

    new_main

--- main2.py
+++ main2.py
@@ -14,7 +14,7 @@ API_TOKEN = config['main']['api_token']
 
 WEBHOOK_HOST = '3.80.107.93'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
-WEBHOOK_LISTEN = '172.17.0.1'  # In some VPS you may need to put here the IP addr
+WEBHOOK_LISTEN = '10.0.0.92'  # In some VPS you may need to put here the IP addr
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
 WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
